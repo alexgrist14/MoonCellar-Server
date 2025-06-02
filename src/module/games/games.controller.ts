9a8944b302f581eb358effb66ee955ca5f9dadee
SergeyHist <sergeyhist@yahoo.com>
2025-06-02 17:23:28 +0300

    feat: update playthroughs

--- src/module/games/games.controller.ts
+++ src/module/games/games.controller.ts
@@ -1,34 +1,67 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Put,
+  Query,
   UseGuards,
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { ApiCookieAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
-
 import { UserIdGuard } from "../auth/user.guard";
-
-import { SavePlaythroughDTO } from "./dto/save-playthrough.dto";
 import { GamesService } from "./games.service";
-import { Roles } from "../roles/roles.decorator";
-import { Role } from "../roles/enums/role.enum";
-import { RolesGuard } from "../roles/roles.guard";
+import {
+  GetPlaythroughsDTO,
+  SavePlaythroughDTO,
+  UpdatePlaythroughDTO,
+} from "./dto/playthrough.dto";
+import mongoose from "mongoose";
 
 @ApiTags("Games")
 @Controller("games")
 export class GamesController {
   constructor(private readonly service: GamesService) {}
 
-  @Post("/save")
+  @Get("/playthroughs")
+  @ApiOperation({ summary: "Get playthroughs" })
+  @ApiCookieAuth()
+  @UseGuards(AuthGuard("jwt"), UserIdGuard)
+  async getPlaythroughsController(@Query() dto: GetPlaythroughsDTO) {
+    return this.service.getPlaythroughs(dto);
+  }
+
+  @Post("/save-playthrough")
   @ApiOperation({ summary: "Save playthrough" })
   @ApiCookieAuth()
-  @UseGuards(AuthGuard("jwt"), UserIdGuard, RolesGuard)
-  @Roles(Role.User)
+  @UseGuards(AuthGuard("jwt"), UserIdGuard)
   @HttpCode(HttpStatus.OK)
   async savePlaythroughController(@Body() dto: SavePlaythroughDTO) {
     return this.service.savePlaythrough(dto);
+  }
+
+  @Put("/update-playthrough/:userId/:id")
+  @ApiOperation({ summary: "Update playthrough" })
+  @ApiCookieAuth()
+  @UseGuards(AuthGuard("jwt"), UserIdGuard)
+  @HttpCode(HttpStatus.OK)
+  async updatePlaythroughController(
+    @Param("id") id: string,
+    @Body() dto: UpdatePlaythroughDTO
+  ) {
+    return this.service.updatePlaythrough(new mongoose.Types.ObjectId(id), dto);
+  }
+
+  @Delete("/delete-playthrough/:userId/:id")
+  @ApiOperation({ summary: "Delete playthrough" })
+  @ApiCookieAuth()
+  @UseGuards(AuthGuard("jwt"), UserIdGuard)
+  @HttpCode(HttpStatus.OK)
+  async deletePlaythroughController(@Param("id") id: string) {
+    return this.service.deletePlaythrough(new mongoose.Types.ObjectId(id));
   }
 }
